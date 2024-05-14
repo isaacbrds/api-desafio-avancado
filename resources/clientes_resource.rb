5f6e84f4be2fdf38f2bd9bc1f451752bd06ca2e8
isaac <isaac.brigido@gmail.com>
2024-05-14 18:26:52 -0300

    feat: Adicionando erros e novos recursos e serviços

--- resources/clientes_resource.rb
+++ resources/clientes_resource.rb
@@ -1,11 +1,64 @@
 require_relative '../servicos/clientes_servico'
 require_relative '../servicos/obj_para_json_servico'
+require_relative '../entidades/cliente'
+require_relative '../servicos/erros/entidade_validacao_erro'
 
 get '/clientes' do
     content_type :json # Set response content type to JSON
-    clientes = ClienteServico.new.todos
+    pagina = request.params['pagina']
+    clientes = ClienteServico.new.todos(pagina)
     # require 'byebug'
 
     # debugger
     ObjParaJsonServico.transformar_para_json(clientes)
+end
+
+
+post '/clientes' do
+    content_type :json # Set response content type to JSON
+    dados_cliente = JSON.parse(request.body.read)
+    novo_cliente = Cliente.new
+    novo_cliente.id = dados_cliente['id']
+    novo_cliente.nome = dados_cliente['nome']
+    novo_cliente.telefone = dados_cliente['telefone']
+    novo_cliente.observacao = dados_cliente['observacao']
+    
+    begin 
+      cliente = ClienteServico.new.salvar(novo_cliente)
+      status 201
+      cliente.to_hash.to_json
+    rescue EntidadeValidacaoErro => e
+      status 400
+      { erro: e.message }.to_json
+    end
+    # require 'byebug'
+
+    # debugger
+end
+
+delete '/clientes/:id' do
+  content_type :json
+  id = params[:id].to_i
+  if id < 1
+    status 400
+    { erro: "O id é obrigatório" }.to_json
+  else
+    ClienteServico.new.excluir_por_id(id)
+    status 204
+    {}
+  end
+end
+
+get '/clientes/:id' do
+  content_type :json
+  id = params[:id].to_i
+  if id < 1
+    status 400
+    { erro: "O id é obrigatório" }.to_json
+  else
+    cliente = ClienteServico.new.buscar_por_id(id)
+    status 200
+    cliente_hash = cliente.to_hash
+    cliente_hash.to_json 
+  end
 end
--- servicos/clientes_servico.rb
+++ servicos/clientes_servico.rb
@@ -7,9 +7,13 @@ class ClienteServico
     conectar_mysql
   end
 
-  def todos
+  def todos(pagina)
+    pagina = 1 if pagina.to_i < 1
+    limit = 5 
+    offset = (pagina.to_i - 1) * limit
+
     # Query to fetch all clients
-    query = 'SELECT * FROM clientes'
+    query = "SELECT * FROM clientes limit #{limit} offset #{offset}"
     # Execute the query
     resultado = @mysql.query(query)
     clientes = []
@@ -24,6 +28,43 @@ class ClienteServico
 
     # Convert the result to an array of hashes
     clientes
+  end
+
+  def salvar(cliente)
+    raise EntidadeValidacaoErro.new("O nome tem que ser preenchido") if(cliente.nome.empty? || cliente.nome.nil?)
+
+    if cliente.id.nil?
+      # require 'byebug'
+      # debugger
+      query = "INSERT INTO clientes (nome, telefone, observacao, created_at, updated_at) VALUES (? , ?, ?, now(), now())"
+      statement = @mysql.prepare(query)
+      statement.execute(cliente.nome, cliente.telefone, cliente.observacao)
+      cliente.id = statement.last_id
+    else
+      query = "UPDATE clientes SET nome = ?, telefone = ?, observacao = ? WHERE id = #{cliente.id}"
+      statement = @mysql.prepare(query)
+      statement.execute(cliente.nome, cliente.telefone, cliente.observacao)
+    end
+
+    cliente
+  end
+
+  def excluir_por_id(id)
+    query = "DELETE FROM clientes WHERE id = #{id}"
+    @mysql.query(query)
+  end
+
+  def buscar_por_id(id)
+    query = "SELECT * FROM clientes WHERE id = #{id.to_i}"
+    resultado = @mysql.query(query)
+    resultado.each do |linha|
+      @cliente = Cliente.new
+      @cliente.id = linha['id']
+      @cliente.nome = linha['nome']
+      @cliente.telefone = linha['telefone']
+      @cliente.observacao = linha['observacao']
+    end
+    @cliente
   end
 
   private 
